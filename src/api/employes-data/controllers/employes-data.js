550179b8246ebaa7ddb5ba963b3dadfe3cef79e4
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-21 22:57:25 +0500

    update and delte the employees

--- src/api/employes-data/controllers/employes-data.js
+++ src/api/employes-data/controllers/employes-data.js
@@ -62,5 +62,89 @@ module.exports = createCoreController(
         return response;
       }
     },
+
+    async update(ctx) {
+      if (ctx.state.user.role.name === "admin") {
+        const response = await super.update(ctx);
+        return response;
+      } else if (ctx.state.user.role.name === "manager") {
+        const userId = ctx.state.user.id;
+        const hotel = await strapi.entityService.findMany(
+          "api::hotel-name.hotel-name",
+          {
+            filters: {
+              manager: userId,
+            },
+          }
+        );
+
+        const hotelId = hotel[0].id;
+
+        console.log("employeeId", hotelId);
+        // Check if the employee belongs to the manager's hotel
+        const employeeId = ctx.params.id;
+        const employee = await strapi.entityService.findOne(
+          "api::employes-data.employes-data",
+          employeeId,
+          {
+            populate: { hotel_name: true },
+          }
+        );
+
+        if (employee.hotel_name.id !== hotelId) {
+          return ctx.unauthorized(
+            "You cannot update an employee that does not belong to your hotel"
+          );
+        }
+
+        // Proceed with the update
+        const response = await super.update(ctx);
+        return response;
+      } else {
+        return ctx.unauthorized(
+          "You are not authorized to update employee data"
+        );
+      }
+    },
+    async delete(ctx) {
+      if (ctx.state.user.role.name === "admin") {
+        const response = await super.delete(ctx);
+
+        return response;
+      } else if (ctx.state.user.role.name === "manager") {
+        const userId = ctx.state.user.id;
+        const hotel = await strapi.entityService.findMany(
+          "api::hotel-name.hotel-name",
+          {
+            filters: {
+              manager: userId,
+            },
+          }
+        );
+
+        const hotelId = hotel[0].id;
+        const employeeId = ctx.params.id;
+        const employee = await strapi.entityService.findOne(
+          "api::employes-data.employes-data",
+          employeeId,
+          {
+            populate: { hotel_name: true },
+          }
+        );
+
+        if (employee.hotel_name.id !== hotelId) {
+          return ctx.unauthorized(
+            "You cannot delete an employee that does not belong to your hotel"
+          );
+        }
+
+        const response = await super.delete(ctx);
+        return response;
+      } else {
+        return ctx.unauthorized(
+          "You are not authorized to delete employee data"
+        );
+      }
+    },
   })
 );
