a78230681e9d21f18bb77ac779317458ffa41bfe
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-23 19:42:35 +0500

    add .temp file

--- src/api/daily-register/controllers/daily-register.js
+++ src/api/daily-register/controllers/daily-register.js
@@ -1,9 +1,46 @@
-'use strict';
+"use strict";
 
 /**
  * daily-register controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::daily-register.daily-register');
+module.exports = createCoreController(
+  "api::daily-register.daily-register",
+  ({ strapi }) => ({
+    async find(ctx) {
+      if (ctx.state.user?.role.name === "admin") {
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+      } else if (ctx.state.user.role.name === "manager") {
+        //hotelid
+        const userId = ctx.state.user.id;
+        const hotel = await strapi.entityService.findMany(
+          "api::hotel-name.hotel-name",
+          {
+            filters: {
+              manager: userId,
+            },
+          }
+        );
+        const { date } = ctx.query.filters;
+        const hotelId = hotel[0].id;
+        const employee = await strapi.entityService.findMany(
+          "api::daily-register.daily-register",
+          {
+            filters: {
+              hotel_name: hotelId,
+              date: date,
+            },
+            populate: "*",
+            sort: "date:desc",
+          }
+        );
+
+        console.log("hotel", hotelId);
+        return employee;
+      }
+    },
+  })
+);
