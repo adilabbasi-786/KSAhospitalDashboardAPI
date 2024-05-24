c0cce33a3829501887a83dda272cc0875ea48c4e
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-24 22:23:51 +0500

    changes in middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,18 +2,7 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      headers: "*", // You can specify which headers are allowed if needed
-      origin: [
-        "https://ksa-hotel-admindasboard.vercel.app/",
-        "http://localhost:3000",
-      ], // Add your Vercel frontend domain here
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
