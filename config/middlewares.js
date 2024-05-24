31c7bf7e1bd0d718cf47b2b26a9deca0881f665d
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-24 22:14:22 +0500

    add config in middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*", // You can specify which headers are allowed if needed
+      origin: [
+        "https://your-frontend-domain.vercel.app",
+        "http://localhost:3000",
+      ], // Add your Vercel frontend domain here
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
