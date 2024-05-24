71457dd6fe9c2728a7fef74f98ab6ff341722f7c
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-24 12:23:10 +0500

    daily sale, and advance forhotel manager

--- src/api/daily-register/controllers/daily-register.js
+++ src/api/daily-register/controllers/daily-register.js
@@ -42,5 +42,28 @@ module.exports = createCoreController(
         return employee;
       }
     },
+
+    async create(ctx) {
+      if (ctx.state.user?.role.name === "admin") {
+        const { data, meta } = await super.create(ctx);
+        return { data, meta };
+      } else if (ctx.state.user.role.name === "manager") {
+        const userId = ctx.state.user.id;
+        const hotel = await strapi.entityService.findMany(
+          "api::hotel-name.hotel-name",
+          {
+            filters: {
+              manager: userId,
+            },
+          }
+        );
+
+        const hotelId = hotel[0].id;
+
+        ctx.request.body.data.hotel_name = hotelId;
+        const response = await super.create(ctx);
+        return response;
+      }
+    },
   })
 );
--- src/api/daily-sale/controllers/daily-sale.js
+++ src/api/daily-sale/controllers/daily-sale.js
@@ -1,9 +1,66 @@
-'use strict';
+"use strict";
 
 /**
  * daily-sale controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::daily-sale.daily-sale');
+module.exports = createCoreController(
+  "api::daily-sale.daily-sale",
+  ({ strapi }) => ({
+    async find(ctx) {
+      if (ctx.state.user?.role.name === "admin") {
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+      } else if (ctx.state.user.role.name === "manager") {
+        const userId = ctx.state.user.id;
+        const hotel = await strapi.entityService.findMany(
+          "api::hotel-name.hotel-name",
+          {
+            filters: {
+              manager: userId,
+            },
+          }
+        );
+        const { date } = ctx.query.filters;
+        const hotelId = hotel[0].id;
+        const sales = await strapi.entityService.findMany(
+          "api::daily-sale.daily-sale",
+          {
+            filters: {
+              hotel_name: hotelId,
+              date: date,
+            },
+            populate: "*",
+            sort: "date:desc",
+          }
+        );
+
+        return sales;
+      }
+    },
+    async create(ctx) {
+      if (ctx.state.user?.role.name === "admin") {
+        const { data, meta } = await super.create(ctx);
+        return { data, meta };
+      } else if (ctx.state.user.role.name === "manager") {
+        const userId = ctx.state.user.id;
+        const hotel = await strapi.entityService.findMany(
+          "api::hotel-name.hotel-name",
+          {
+            filters: {
+              manager: userId,
+            },
+          }
+        );
+
+        const hotelId = hotel[0].id;
+
+        ctx.request.body.data.hotel_name = hotelId;
+        const response = await super.create(ctx);
+        return response;
+      }
+    },
+  })
+);
