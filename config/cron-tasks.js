60fb77f9bfe8c8804b597a4ef7cf23201aa9277e
adilabbasi-786 <m.adilabbasi786@gmail.com>
2024-05-30 16:44:00 +0500

    set notification changes

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -7,7 +7,8 @@ module.exports = {
       const hotels = await strapi.entityService.findMany(
         "api::hotel-name.hotel-name"
       );
-      hotels.forEach(async (hotel) => {
+
+      for (const hotel of hotels) {
         const hotelId = hotel.id;
         const hotelname = hotel.name;
         console.log("Hotel ID:", hotelId);
@@ -22,31 +23,46 @@ module.exports = {
             populate: "*",
           }
         );
-        console.log("expanse length", expenses);
 
-        if (expenses.length === 0) {
+        const sale = await strapi.entityService.findMany(
+          "api::daily-sale.daily-sale",
+          {
+            filters: {
+              hotel_name: hotelId,
+              date: currentDate,
+            },
+            populate: "*",
+          }
+        );
+
+        console.log("expenses length", expenses.length);
+        console.log("sales length", sale.length);
+
+        if (expenses.length === 0 && sale.length === 0) {
           const createNotification = await strapi.entityService.create(
             "api::notification.notification",
             {
               data: {
-                message: `manager of ${hotelname} does not enter record of ${currentDate} `,
+                message: `Manager of ${hotelname} has not entered records for ${
+                  currentDate.toISOString().split("T")[0]
+                }`,
                 read: false,
                 publishedAt: new Date(),
-                hotel_name: 56,
+                hotel_name: hotelId,
               },
             }
           );
           console.log("createNotification", createNotification);
           console.log("No record entered for hotel:", hotelname);
         } else {
-          console.log("Expenses for hotel:", hotelname, expenses);
+          console.log("Records exist for hotel:", hotelname);
         }
-      });
+      }
 
       console.log("Hotels:", hotels);
     },
     options: {
-      rule: "0 * * * *",
+      rule: "0 0 * * *", // This will run the task every day at midnight
       tz: "Asia/Riyadh",
     },
   },
